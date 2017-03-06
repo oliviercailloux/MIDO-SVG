101aca144235725bd8aa9de57d91f1e01765541a
Romano <romain.scohy@dauphine.eu>
2017-03-06 21:02:19 +0100

    Ajout constructeur vides

--- cocolollipop.s1.testSVGgen/src/univ/Formation.java
+++ cocolollipop.s1.testSVGgen/src/univ/Formation.java
@@ -1,5 +1,8 @@
 package univ;
 
 public abstract class Formation {
-	// La classe emre qui represente une formation mais qui est abstraite : Soit on est en Licence, soit en Master
+	// La classe mere qui represente une formation mais qui est abstraite : Soit on est en Licence, soit en Master
+	public Formation(){
+		
+	}
 }
--- cocolollipop.s1.testSVGgen/src/univ/Master.java
+++ cocolollipop.s1.testSVGgen/src/univ/Master.java
@@ -1,5 +1,7 @@
 package univ;
 
 public class Master extends Formation{
-
+	public Master(){
+		super();
+	}
 }
