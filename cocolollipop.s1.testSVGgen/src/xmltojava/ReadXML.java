0d4c625628caa4f6d6202fbad2212e4b5bac6a37
Romano <romain.scohy@dauphine.eu>
2017-03-08 21:31:31 +0100

    Creation du main XML reader

---
+++ cocolollipop.s1.testSVGgen/src/xmltojava/ReadXML.java
@@ -0,0 +1,17 @@
+package xmltojava;
+
+public class ReadXML {
+	/**
+	 * The aim of this class is to get data form an XML FILE in static mode
+	 * In a second time we will create a function which can used in the main program, in order to create
+	 * multiple objects such as Enseignant, Matiere, Formation (package univ)
+	 * 
+	 * We will use a DOM parser
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
