a3439efe4d48431489ae86981e0cf4a1464fb428
Caroline Vong <m1105.102.cocolollipop@gmail.com>
2017-03-08 22:55:04 +0100

    ajout des traitsé

--- V1/src/cocolollipop/master/MasterSVGGen.java
+++ V1/src/cocolollipop/master/MasterSVGGen.java
@@ -1,10 +1,7 @@
 package cocolollipop.master;
 
 import java.awt.Color;
-import java.awt.Rectangle;
 import java.awt.Dimension;
-import java.awt.BasicStroke;
-import java.awt.Stroke;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -51,78 +48,85 @@ public class MasterSVGGen {
 
 		// Ask the test to render into the SVG Graphics2D implementation.
 		g.setPaint(Color.black);
-		int dimX=15000;
-		int dimY=15000;
+		int dimX = 15000;
+		int dimY = 15000;
 		g.setSVGCanvasSize(new Dimension(dimX, dimY));
 		g.drawString("MIDO", 300, 20);
 		g.drawString("Masters", 300, 50);
-		
+
 		/**
-		 * Partie INFO- ORG
+		 * Part INFO- ORG
 		 */
-		g.drawString("INFO-ORG", 100,75 );
-		
-		ArrayList<String> master = new ArrayList<String>();
-		master.add("M1");
-		master.add("M2");
-		ArrayList<String> m1iofil  = new ArrayList<String>();
-		m1iofil.add("MIAGE classique");
-		m1iofil.add("MIAGE apprentissage");
-		m1iofil.add("Informatique de décision");
-		
-		ArrayList<String> io_m2_fil  = new ArrayList<String>();
+		g.drawString("INFO-ORG", 100, 75);
+
+		ArrayList<String> io_master = new ArrayList<String>();
+		io_master.add("M1");
+		io_master.add("M2");
+		ArrayList<String> io_m1_fil = new ArrayList<String>();
+		io_m1_fil.add("MIAGE classique");
+		io_m1_fil.add("MIAGE apprentissage");
+		io_m1_fil.add("Informatique de décision");
+
+		ArrayList<String> io_m2_fil = new ArrayList<String>();
 		io_m2_fil.add("MODO");
 		io_m2_fil.add("ISI");
-		
-		ArrayList<String> io_m2_isi_fil  = new ArrayList<String>();
+
+		ArrayList<String> io_m2_isi_fil = new ArrayList<String>();
 		io_m2_isi_fil.add("MIAGE-IF");
 		io_m2_isi_fil.add("MIAGE-STIN");
 		io_m2_isi_fil.add("MIAGE-ID");
-				
-		for(int i = 0; i<master.size(); i++){
-			g.drawString(master.get(i), 30 , 180+ i*300);	
+
+		for (int i = 0; i < io_master.size(); i++) {
+			g.drawString(io_master.get(i), 30, 180 + i * 300);
 		}
-		
-		for(int i = 0; i<m1iofil.size(); i++){
-			g.drawString(m1iofil.get(i), 60 , 150+ i*30);	
+
+		for (int i = 0; i < io_m1_fil.size(); i++) {
+			g.drawString(io_m1_fil.get(i), 60, 150 + i * 30);
+			g.drawLine(50, 180, 60, 150 + i * 30);
 		}
-		for(int i = 0; i<io_m2_fil.size(); i++){
-			g.drawString(io_m2_fil.get(i), 60, 450+ i*60);	
+		for (int i = 0; i < io_m2_fil.size(); i++) {
+			g.drawString(io_m2_fil.get(i), 60, 450 + i * 60);
+			g.drawLine(50, 480, 60, 450 + i * 60);
+			
 		}
-		
-		for(int i = 0; i<io_m2_isi_fil.size(); i++){
-			g.drawString(io_m2_isi_fil.get(i), 175, 485+ i*30);	
-			g.drawString("Apprentissage", 300, 485+ i*30);	
+
+		for (int i = 0; i < io_m2_isi_fil.size(); i++) {
+			g.drawString(io_m2_isi_fil.get(i), 175, 485 + i * 30);
+			g.drawString("Apprentissage", 300, 485 + i * 30);
+			g.drawLine(185, 485 + i * 30, 310, 485 + i * 30);
+			g.drawLine(80,510, 175, 485 + i * 30);
 		}
-		
+
 		/**
-		 * Partie MATHS-APP
+		 * Part MATHS-APP
 		 */
-		g.drawString("MATHS-APP", 500,75 );	
+		g.drawString("MATHS-APP", 500, 75);
 		ArrayList<String> ma_master = new ArrayList<String>();
 		ma_master.add("M1");
 		ma_master.add("M2");
-		ArrayList<String> ma_m1_fil  = new ArrayList<String>();
+		ArrayList<String> ma_m1_fil = new ArrayList<String>();
 		ma_m1_fil.add("Actuariat");
 		ma_m1_fil.add("Maths approfondies");
 		ma_m1_fil.add("Méth maths en éco");
-		
-		ArrayList<String> ma_m2_fil  = new ArrayList<String>();
+
+		ArrayList<String> ma_m2_fil = new ArrayList<String>();
 		ma_m2_fil.add("Actuariat");
 		ma_m2_fil.add("etc");
-		
-		for(int i = 0; i<ma_master.size(); i++){
-			g.drawString(ma_master.get(i), 500 , 180+ i*300);	
+
+		for (int i = 0; i < ma_master.size(); i++) {
+			g.drawString(ma_master.get(i), 500, 180 + i * 300);
 		}
-		
-		for(int i = 0; i<ma_m1_fil.size(); i++){
-			g.drawString(ma_m1_fil.get(i), 600 , 150+ i*30);	
+
+		for (int i = 0; i < ma_m1_fil.size(); i++) {
+			g.drawString(ma_m1_fil.get(i), 600, 150 + i * 30);
+			g.drawLine(510, 180, 600, 150 + i * 30);
 		}
-		
-		for(int i = 0; i<ma_m2_fil.size(); i++){
-			g.drawString(ma_m2_fil.get(i), 600, 450+ i*60);	
+
+		for (int i = 0; i < ma_m2_fil.size(); i++) {
+			g.drawString(ma_m2_fil.get(i), 600, 450 + i * 60);
+			g.drawLine(510, 480,600, 450 + i * 60);
 		}
-		
+
 		// Finally, stream out SVG using UTF-8 encoding.
 		boolean useCSS = true; // we want to use CSS style attributes
 		try (Writer out = new OutputStreamWriter(new FileOutputStream("outMaster.svg"), "UTF-8")) {
