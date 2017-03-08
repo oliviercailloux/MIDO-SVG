fa9c39dcb199dae3b593d8ce01b6e2e0136cace4
Romano <romain.scohy@dauphine.eu>
2017-03-08 23:10:56 +0100

    Add 2 methods in order to get Data

--- cocolollipop.s1.testSVGgen/src/univ/Enseignant.java
+++ cocolollipop.s1.testSVGgen/src/univ/Enseignant.java
@@ -2,4 +2,37 @@ package univ;
 
 public class Enseignant {
 	private String nomEnseignant;
+	private String prenomEnseignant;
+	private String phone;
+	private String address;
+	
+	/*
+	 * Getters And Setters
+	 */
+	public String getNomEnseignant() {
+		return nomEnseignant;
+	}
+	public void setNomEnseignant(String nomEnseignant) {
+		this.nomEnseignant = nomEnseignant;
+	}
+	public String getPrenomEnseignant() {
+		return prenomEnseignant;
+	}
+	public void setPrenomEnseignant(String prenomEnseignant) {
+		this.prenomEnseignant = prenomEnseignant;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 }
--- cocolollipop.s1.testSVGgen/src/xmltojava/ReadXML.java
+++ cocolollipop.s1.testSVGgen/src/xmltojava/ReadXML.java
@@ -13,6 +13,8 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import univ.Enseignant;
+
 public class ReadXML {
 	/**
 	 * The aim of this class is to get data form an XML FILE in static mode
@@ -47,16 +49,36 @@ public class ReadXML {
 		// We show node Root Name !
 		System.out.println(root.getNodeName());
 		
-		// We show 
-		for (int i = 0; i<nbRootNodesList; i++) {
-		    if(childNodesList.item(i).getNodeType() == Node.ELEMENT_NODE) {
-		         Node person = childNodesList.item(i);
-		        System.out.println(person.getNodeName());
-		    }				
-		}
+		// We show the nodeChild of the Root Node
+	    for (int i = 0; i<nbRootNodesList; i++) {
+	        if(childNodesList.item(i).getNodeType() == Node.ELEMENT_NODE) {
+	            Element person = (Element) childNodesList.item(i);
+				
+		    	// We show the sex of the person ; using getAttributte Method
+			    System.out.println("\nThe PERSON");
+			    System.out.println("sex : " + person.getAttribute("sexe"));
+				
+		    	// We use getElementsByTagName in order to get the name and the surname of the person
+			    Element name = (Element) person.getElementsByTagName("nom").item(0);
+			    Element surname = (Element) person.getElementsByTagName("prenom").item(0);
+						
+			    //Affichage du nom et du prénom
+			    System.out.println("nom : " + name.getTextContent());
+			    System.out.println("prénom : " + surname.getTextContent());
+	        }
+	    }
+		
+		
+		
 
 		
+		// We set attribute to the Enseignant object
+		//Enseignant firstEnseignant = new Enseignant();
+		//firstEnseignant.setNomEnseignant(s);
+		
+		
 		}
+		
 		catch (final ParserConfigurationException e) {
 			System.out.println("Attention, error from DocumentBuilder : parser isn't configured");
 		    e.printStackTrace();
