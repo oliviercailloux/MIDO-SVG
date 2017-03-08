a1857c3309013a87a40198a919cc041776cb7ea9
Caroline Vong <m1105.102.cocolollipop@gmail.com>
2017-03-08 23:40:05 +0100

    licence en cours

--- V1/src/cocolollipop/svgGen/LicenceSVGGen.java
+++ V1/src/cocolollipop/svgGen/LicenceSVGGen.java
@@ -1,4 +1,4 @@
-package cocolollipop.master;
+package cocolollipop.svgGen;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -23,10 +23,10 @@ import org.w3c.dom.Document;
  *
  */
 
-public class MasterSVGGen {
+public class LicenceSVGGen {
 
 	public static void main(String[] args) throws Exception {
-		MasterSVGGen test = new MasterSVGGen();
+		LicenceSVGGen test = new LicenceSVGGen();
 		test.paint();
 	}
 
@@ -67,14 +67,17 @@ public class MasterSVGGen {
 		io_m1_fil.add("MIAGE apprentissage");
 		io_m1_fil.add("Informatique de décision");
 
-		ArrayList<String> io_m2_fil = new ArrayList<String>();
-		io_m2_fil.add("MODO");
-		io_m2_fil.add("ISI");
+		ArrayList<String> l3_fil = new ArrayList<String>();
+		l3_fil.add("Maths appliquées");
+		l3_fil.add("Informatique des Organisations");
 
-		ArrayList<String> io_m2_isi_fil = new ArrayList<String>();
-		io_m2_isi_fil.add("MIAGE-IF");
-		io_m2_isi_fil.add("MIAGE-STIN");
-		io_m2_isi_fil.add("MIAGE-ID");
+		ArrayList<String> l3_io_fil = new ArrayList<String>();
+		l3_io_fil.add("MIAGE-Apprentissage");
+		l3_io_fil.add("MIAGE-Décision");
+		
+		ArrayList<String> l3_ma_fil = new ArrayList<String>();
+		l3_io_fil.add("Maths-éco");
+		l3_io_fil.add("Maths-info");
 
 		for (int i = 0; i < io_master.size(); i++) {
 			g.drawString(io_master.get(i), 30, 180 + i * 300);
@@ -84,52 +87,26 @@ public class MasterSVGGen {
 			g.drawString(io_m1_fil.get(i), 60, 150 + i * 30);
 			g.drawLine(50, 180, 60, 150 + i * 30);
 		}
-		for (int i = 0; i < io_m2_fil.size(); i++) {
-			g.drawString(io_m2_fil.get(i), 60, 450 + i * 60);
+		for (int i = 0; i < l3_fil.size(); i++) {
+			g.drawString(l3_fil.get(i), 60, 450 + i * 60);
 			g.drawLine(50, 480, 60, 450 + i * 60);
 			
 		}
 
-		for (int i = 0; i < io_m2_isi_fil.size(); i++) {
-			g.drawString(io_m2_isi_fil.get(i), 175, 485 + i * 30);
-			g.drawString("Apprentissage", 300, 485 + i * 30);
-			g.drawLine(185, 485 + i * 30, 310, 485 + i * 30);
-			g.drawLine(80,510, 175, 485 + i * 30);
+		for (int i = 0; i < l3_io_fil.size(); i++) {
+			g.drawString(l3_io_fil.get(i), 175, 500 + i * 30);
+			g.drawLine(80,510, 175, 500 + i * 30);
 		}
-
-		/**
-		 * Part MATHS-APP
-		 */
-		g.drawString("MATHS-APP", 500, 75);
-		ArrayList<String> ma_master = new ArrayList<String>();
-		ma_master.add("M1");
-		ma_master.add("M2");
-		ArrayList<String> ma_m1_fil = new ArrayList<String>();
-		ma_m1_fil.add("Actuariat");
-		ma_m1_fil.add("Maths approfondies");
-		ma_m1_fil.add("Méth maths en éco");
-
-		ArrayList<String> ma_m2_fil = new ArrayList<String>();
-		ma_m2_fil.add("Actuariat");
-		ma_m2_fil.add("etc");
-
-		for (int i = 0; i < ma_master.size(); i++) {
-			g.drawString(ma_master.get(i), 500, 180 + i * 300);
-		}
-
-		for (int i = 0; i < ma_m1_fil.size(); i++) {
-			g.drawString(ma_m1_fil.get(i), 600, 150 + i * 30);
-			g.drawLine(510, 180, 600, 150 + i * 30);
-		}
-
-		for (int i = 0; i < ma_m2_fil.size(); i++) {
-			g.drawString(ma_m2_fil.get(i), 600, 450 + i * 60);
-			g.drawLine(510, 480,600, 450 + i * 60);
+		
+		for (int i = 0; i < l3_ma_fil.size(); i++) {
+			g.drawString(l3_ma_fil.get(i), 175, 500 + i * 30);
+			g.drawLine(10,450, 175, 30 + i * 30);
+			
 		}
 
 		// Finally, stream out SVG using UTF-8 encoding.
 		boolean useCSS = true; // we want to use CSS style attributes
-		try (Writer out = new OutputStreamWriter(new FileOutputStream("outMaster.svg"), "UTF-8")) {
+		try (Writer out = new OutputStreamWriter(new FileOutputStream("outLicence.svg"), "UTF-8")) {
 			g.stream(out, useCSS);
 		}
 	}
