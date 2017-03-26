6f434e3572f298bc0b16ddf04ab5c47699501336
Caroline Vong <m1105.102.cocolollipop@gmail.com>
2017-03-26 17:26:42 +0200

    petites rectifications

--- com.github.cocolollipop.mido-svg.project/src/com/github/cocolollipop/GenerateAuto/DessinLicence.java
+++ com.github.cocolollipop.mido-svg.project/src/com/github/cocolollipop/GenerateAuto/DessinLicence.java
@@ -43,52 +43,51 @@ public class DessinLicence {
 	 * @param nbCol
 	 */
 	public void getDecalage(int nbRow, int nbCol) {
-		this.decalageX = this.dimXCanvas / (nbCol+1);
-		this.decalageY = this.dimYCanvas / (nbRow+1);
+		this.decalageX = this.dimXCanvas / (nbCol + 1);
+		this.decalageY = this.dimYCanvas / (nbRow + 1);
 	}
 
-	public SVGGraphics2D paint(ArrayList<Licence> listOfAllLicences) throws IOException, ParserConfigurationException {
+	public void paint(ArrayList<Licence> listOfAllLicences) throws IOException, ParserConfigurationException {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
 
 		// Get a DOMImplementation.
 		DOMImplementation domImpl = db.getDOMImplementation();
-
 		// Create an instance of org.w3c.dom.Document.
 		String svgNS = "http://www.w3.org/2000/svg";
 		Document document = domImpl.createDocument(svgNS, "svg", null);
-
 		// Create an instance of the SVG Generator.
 		SVGGeneratorContext ctx = SVGGeneratorContext.createDefault(document);
 		ctx.setEmbeddedFontsOn(true);
 		SVGGraphics2D g = new SVGGraphics2D(ctx, false);
-
-		// Ask the test to render into the SVG Graphics2D implementation.
-
-		// On va d'abord fixer la dimension du plan de travails
+		// set the size of SVG
 		g.setSVGCanvasSize(new Dimension(this.dimXCanvas, this.dimYCanvas));
-
-		// On fixe la couleur du pinceau � noir
-		g.setPaint(Color.black);
-
-		// On cr�e 3 objets : Licence 1, 2 et 3
-
+		g.setPaint(Color.black); // paint in black
+		/**
+		 * We starts painting
+		 */
 		g.drawString("MIDO", 300, 20);
 		g.drawString("Masters", 300, 50);
-		for(int i=0;i<listOfAllLicences.size();i++){
-			g.drawString(listOfAllLicences.get(i).getIntitule(),listOfAllLicences.get(i).getPosX(), listOfAllLicences.get(i).getPosY());
+		/**
+		 * We creates the licences
+		 */
+		for (int i = 0; i < listOfAllLicences.size(); i++) {
+			g.drawString(listOfAllLicences.get(i).getIntitule(), listOfAllLicences.get(i).getPosX(),
+					listOfAllLicences.get(i).getPosY());
 		}
+		
+		/**
+		 * Exports the SVG
+		 */
 		try (Writer out = new OutputStreamWriter(new FileOutputStream("DessinLicenceTest.svg"), "UTF-8")) {
 			g.stream(out, true);
 		}
-		
-		return g;
 	}
 
 	public static void main(String[] args) throws Exception {
 		DessinLicence test = new DessinLicence();
 
-		/**We are asking how many rows and how many columns we need**/
+		/** We are asking how many rows and how many columns we need **/
 		int nbCol = test.askLicenceNb();
 		int nbRows = test.askYearNb();
 
@@ -101,9 +100,9 @@ public class DessinLicence {
 		// On cr�e nos nbRows*nbCol Licences
 		for (int i = 0; i < nbCol; i++) {
 			for (int j = 0; j < nbRows; j++) {
-				Licence myLicence = new Licence(j, "Licence TEST :" + j + " Annee : " + (i+1));
-				myLicence.setPosX(test.decalageX,i);
-				myLicence.setPosY(test.decalageY,j);
+				Licence myLicence = new Licence(j, "Licence TEST :" + j + " Annee : " + (i + 1));
+				myLicence.setPosX(test.decalageX, i);
+				myLicence.setPosY(test.decalageY, j);
 				listOfAllLicences.add(myLicence);
 			}
 
@@ -115,27 +114,26 @@ public class DessinLicence {
 		System.out.println("J'ai " + nbLicences + " Licences au total");
 		test.paint(listOfAllLicences);
 
-
 	}
 
 	private int askLicenceNb() {
 		System.out.println("Combien de Licences souhaitez-vous afficher ? ");
 		Scanner sc = new Scanner(System.in);
 		try {
-		// ici faire un test sur le parseInt
-		String entry = sc.nextLine();
-		int nbCol = Integer.parseInt(entry);
-		return nbCol;
+			// ici faire un test sur le parseInt
+			String entry = sc.nextLine();
+			int nbCol = Integer.parseInt(entry);
+			return nbCol;
 		} finally {
 			if (sc != null) {
-				//sc.close();
+				// sc.close();
 			}
 		}
 
 	}
 
 	private int askYearNb() {
-		
+
 		System.out.println("Combien d'ann�es souhaitez-vous afficher ? ");
 		Scanner sc = new Scanner(System.in);
 		try {
@@ -145,7 +143,7 @@ public class DessinLicence {
 			return nbRow;
 		} finally {
 			if (sc != null) {
-			//sc.close();
+				// sc.close();
 			}
 		}
 
