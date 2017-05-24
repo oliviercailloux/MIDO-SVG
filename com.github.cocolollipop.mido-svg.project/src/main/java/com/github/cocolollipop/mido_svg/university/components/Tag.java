3dd0ab7c5db54ea9649d1375d4b4c0df6300b424
Romano <romain.scohy@dauphine.eu>
2017-05-24 20:47:32 +0200

    Petite demo pour JAXB :
    En gros, c'est la trame a respecter + les annotations à mettre pour que
    la création d'un XML fonctionne

--- com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/university/components/Tag.java
+++ com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/university/components/Tag.java
@@ -3,13 +3,17 @@ import java.util.List;
 import javax.xml.bind.annotation.*;
 
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Tag {
-	@XmlAttribute
+	@XmlElement(name="name")
 	private String name; // Name of the tag
 	@XmlElement(name="subject")
 	private List<Subject> subjects;
 	
-	
+	public Tag(){
+		this.name = "";
+		this.subjects = null;
+	}
 	public Tag(String name, List<Subject> subjects) {
 		this.name = name;
 		this.subjects = subjects;
