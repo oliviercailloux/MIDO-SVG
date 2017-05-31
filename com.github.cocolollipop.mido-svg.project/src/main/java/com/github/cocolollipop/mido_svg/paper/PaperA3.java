a56c0ff8eb010e0c46f3dcd13de720142b759427
Cocolollipop <m1105.102.cocolollipop@gmail.com>
2017-05-31 02:30:14 +0200

    my bad, petite erreur d'initialisation du paper

--- com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/paper/PaperA3.java
+++ com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/paper/PaperA3.java
@@ -7,5 +7,7 @@ public class PaperA3 extends Paper {
 		this.setFormat(TypeFormat.A3);
 		this.setDimXCanvas(2480);
 		this.setDimYCanvas(3508);
+		this.setCanevasX(2480);
+		this.setCanevasY(3508);
 	}
 }
--- com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/paper/PaperA4.java
+++ com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/paper/PaperA4.java
@@ -7,6 +7,8 @@ public class PaperA4 extends Paper {
 		this.setFormat(TypeFormat.A4);
 		this.setDimXCanvas(3508);
 		this.setDimYCanvas(4961);
+		this.setCanevasX(3508);
+		this.setCanevasY(4961);
 	}
 
 }
