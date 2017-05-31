85a351acb78ca264f8aa99852dec7b5af4a7e091
IkramBouhya <ikrambouhya@gmail.com>
2017-05-31 19:05:19 +0200

    prerequisites

--- com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/svg_generator/DrawerSVGGen.java
+++ com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/svg_generator/DrawerSVGGen.java
@@ -262,6 +262,7 @@ public class DrawerSVGGen {
 	 * @param x2
 	 * @param y2
 	 */
+
 	void drawArrow(Graphics g1, int x1, int y1, int x2, int y2) {
 		Graphics2D g = (Graphics2D) g1.create();
 		int ARR_SIZE = 5;
