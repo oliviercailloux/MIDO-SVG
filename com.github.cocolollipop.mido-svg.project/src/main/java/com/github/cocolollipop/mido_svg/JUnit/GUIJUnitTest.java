b66dbf33a71f509b1dd4fe8955294b7ffb7c400f
IkramBouhya <ikrambouhya@gmail.com>
2017-06-18 16:44:55 +0200

    Ajouter test unitaire de la fonction countFormations

--- com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/JUnit/GUIJUnitTest.java
+++ com.github.cocolollipop.mido-svg.project/src/main/java/com/github/cocolollipop/mido_svg/JUnit/GUIJUnitTest.java
@@ -4,17 +4,31 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
+import javax.xml.crypto.Data;
+
 import org.junit.Test;
 
+import com.github.cocolollipop.mido_svg.model.DataBase;
+import com.github.cocolollipop.mido_svg.svg_generator.ResponsiveSVG;
 import com.github.cocolollipop.mido_svg.view.GUISVGGeneratorbis;
 
 public class GUIJUnitTest {
-
+	
+	
+	/**
+	 * In this function we will test the function that count the number of formations in Parcours (or Year) "M1"
+	 * 
+	 * */ 
+	
 	@Test
 	public void GUItest() throws IOException {
-		GUISVGGeneratorbis gui = new GUISVGGeneratorbis();
-		gui.open();
-		fail("Not yet implemented");
+		
+		
+		ResponsiveSVG r = new ResponsiveSVG();
+		DataBase data = new DataBase();
+		int output = r.countFormations(data.getFormations(), "M1");
+		assertEquals(2, output);
+		
 	}
 
 }
