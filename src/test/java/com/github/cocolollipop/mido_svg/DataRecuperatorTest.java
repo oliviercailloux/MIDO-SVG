74325d80ff364d4b869728edd9f6494c8be5d997
brulej <jeremie.brule@gmail.com>
2020-04-19 16:53:50 +0100

    test

---
+++ src/test/java/com/github/cocolollipop/mido_svg/DataRecuperatorTest.java
@@ -0,0 +1,29 @@
+package com.github.cocolollipop.mido_svg;
+
+import static org.junit.Assert.*;
+
+import com.github.cocolollipop.mido_svg.model.*;
+import org.junit.Test;
+
+public class DataRecuperatorTest {
+
+	@Test
+	public void getSubjectsTest() {
+		List<Subject> list = getSubjects("FRUAI0750736TPRCP1ALUEC1");
+		Assertions.assertEquals(list.size(), 2);
+		
+		Iterator<Subject> l = list.iterator();
+		
+		Subject s1 = l.next();
+		Assertions.assertEquals("Introduction à la microéconomie", s1.getTitle());
+		Assertions.assertEquals(4, s1.getCredit());
+		Assertions.assertEquals("TAN", s1.getResponsible().getLastName());
+		
+		Subject s2 = l.next();
+		Assertions.assertEquals("Anglais 1", s2.getTitle());
+		Assertions.assertEquals(2, s2.getCredit());
+		Assertions.assertEquals("TAN", s2.getResponsible().getLastName());
+	}
+
+
+}
