2ba3415007eaf4f6ccc67faae3bd974f7eff68bc
Arnaud-Pa <83297854+Arnaud-Pa@users.noreply.github.com>
2022-06-21 16:28:58 +0200

    Mise à jour en fonction des commentaires et suppresion de la modification du POM

--- src/main/java/io/github/oliviercailloux/mido_svg/university/components/Teacher.java
+++ src/main/java/io/github/oliviercailloux/mido_svg/university/components/Teacher.java
@@ -3,58 +3,56 @@ package io.github.oliviercailloux.mido_svg.university.components;
 /**
  * This class corresponds to the teacher's details
  */
-public final class Teacher {
+public class Teacher {
 
-  private String address;
+	private String address;
 
-  private String firstName;
+	private String firstName;
 
-  private String lastName;
+	private String lastName;
 
-  private String gender;
+	private String gender;
 
-  private String phone;
+	private String phone;
 
-  public Teacher(String firstName, String lastName, String gender, String address, String phone){
-this.firstName = firstName;
-    this.lastName = lastName;
-    this.gender = gender;
-    this.address = address;
-    this.phone = phone;
+	public Teacher(String firstName, String lastName, String gender, String address, String phone) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.address = address;
+		this.phone = phone;
+	}
 
-  }
+	/*
+	 * Getters
+	 */
 
-  /*
-   * Getters
-   */
+	public String getAddress() {
+		return address;
+	}
 
-  public String getAddress() {
-    return address;
-  }
+	public String getFirstName() {
+		return firstName;
+	}
 
-  public String getFirstName() {
-    return firstName;
-  }
+	public String getLastName() {
+		return lastName;
+	}
 
-  public String getLastName() {
-	    return lastName;
-  }
+	/**
+	 * Returns the full name (LastName + FirstName) of a teacher
+	 * 
+	 */
+	public String getFullNameTeacher() {
+		return lastName + " " + firstName;
+	}
 
-  /**
-   * Returns the full name (LastName + FirstName) of a teacher
-   * 
-   */
-  public String getFullNameTeacher() {
-    return lastName + " " + firstName;
-  }
+	public String getGender() {
+		return gender;
+	}
 
-  public String getGender() {
-    return gender;
-  }
-
-
-  public String getPhone() {
-    return phone;
-  }
+	public String getPhone() {
+		return phone;
+	}
 
 }
